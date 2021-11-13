2e2bd36710bf3618199b3fd76eb4cda0262b9183
tuannnn38 <buithanhtuan9874@gmail.com>
2021-11-13 21:46:13 +0700

    structure folder in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationFormComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+   
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './header.component.html',
+    selector: 'header-component'
+})
+export class HeaderComponent{
+
+}
